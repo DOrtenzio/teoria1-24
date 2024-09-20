ad34e7375205d47d864267dc4a7024596eb0de3a
dortenzio <dortenzio.diego.studente@itispaleocapa.it>
2024-09-20 09:35:28 +0200

    Costruttori

--- src/rettangolo/Rettangolo.java
+++ src/rettangolo/Rettangolo.java
@@ -1,13 +1,21 @@
 package rettangolo;
 
 public class Rettangolo {
+    //Variabili d'istanza
     private double base;
     private double altezza;
 
+    //Metodi
     public void ridimensiona (double baseN,double altezzaN){
         base=baseN;
         altezza=altezzaN;
     }
     public double perimetro (){ return (base*2)+(altezza*2); }
     public double area (){ return base*altezza; }
+
+    //Costruttore
+    public Rettangolo(double baseN, double altezzaN){
+        base=baseN;
+        altezza=altezzaN;
+    }
 }
