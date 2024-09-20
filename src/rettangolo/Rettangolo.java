5e2f87470d594d186117cb7d239538da13948257
dortenzio <dortenzio.diego.studente@itispaleocapa.it>
2024-09-20 09:31:58 +0200

    Prime

--- src/rettangolo/Rettangolo.java
+++ src/rettangolo/Rettangolo.java
@@ -5,6 +5,9 @@ public class Rettangolo {
     private double altezza;
 
     public void ridimensiona (double baseN,double altezzaN){
-
+        base=baseN;
+        altezza=altezzaN;
     }
+    public double perimetro (){ return (base*2)+(altezza*2); }
+    public double area (){ return base*altezza; }
 }
